78897a6f7b8e9158c07a0bf4056e893f4c092c60
9Oc <1987285+9Oc@users.noreply.github.com>
2026-08-20 23:07:04 -0500

    Added iTunes provider

--- tmdbwrapper/tmdbmovie.py
+++ tmdbwrapper/tmdbmovie.py
@@ -174,6 +174,7 @@ class ProviderName(Enum):
     INDIEFLIX = "IndieFlix"
     INFINITY_SELECTION_AMAZON_CHANNEL = "Infinity Selection Amazon Channel"
     IQIYI = "iQIYI"
+    ITUNES = "Apple TV Store"
     ITVX_PREMIUM = "ITVX Premium"
     IWONDER_FULL_AMAZON_CHANNEL = "iWonder Full Amazon Channel"
     JIOHOTSTAR = "JioHotstar"
@@ -564,6 +565,7 @@ class Provider:
         ProviderName.INDIEFLIX.value: {"indieflix"},
         ProviderName.INFINITY_SELECTION_AMAZON_CHANNEL.value: {"infinity selection amazon channel"},
         ProviderName.IQIYI.value: {"iqiyi"},
+        ProviderName.ITUNES.value: {"apple tv store", "appletv store", "apple tvstore", "itunes"},
         ProviderName.ITVX_PREMIUM.value: {"itvx premium"},
         ProviderName.IWONDER_FULL_AMAZON_CHANNEL.value: {"iwonder full amazon channel"},
         ProviderName.JIOHOTSTAR.value: {"jiohotstar"},
